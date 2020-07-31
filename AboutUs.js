2cd0bc64b016287ec0c3041d86164d022f2c5764
Sankalp Anand <sankalpananddd29@gmail.com>
2020-07-31 15:14:58 +0530

    Added AboutUs page codes

---
+++ AboutUs.js
@@ -0,0 +1,14 @@
+("#d3").accordion
+(
+    {
+        collapsible : true,
+        event : "click",
+        animate : 300, 
+        active : 3, 
+        heightStyle : true, 
+        icons : 
+        {
+            header : "ui-icon-locked",activeHeader : "ui-icon-unlocked"
+        } 
+    }
+);
